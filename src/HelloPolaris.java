adfa3440e011a59b3af5b2301664f00e381e6617
Kamrun Foster <kamrun.foster4@gmail.com>
2021-07-29 13:37:51 -0500

    intro to java lecture

---
+++ src/HelloPolaris.java
@@ -0,0 +1,6 @@
+public class HelloPolaris {
+    public static void main(String[] args){
+        System.out.println("Howdy There Polaris!!!"); //strings inside of java require ""
+        //single quotes '' are only for characters
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");// used to output info w/ a newline
+        System.out.print("Hola,");
+        System.out.print("Mundo!");
+    }
+}

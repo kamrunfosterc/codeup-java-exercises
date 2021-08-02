628efe66bbf0a299ac8ca9bdb6f6c8a930ba8ec5
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-02 14:09:49 -0500

    walkthrough complete, minus the bonus which we try next

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -4,39 +4,53 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately " + "%f.", pi);
-        //TODO
-        //NOT RETURNING 3.14 BUT I AM SURE THAT'S A SIMPLE FIX, CAN'T FIND RIGHT NOW
+        System.out.printf("The value of pi is approximately " + "%f.", pi);// my way yield 3.14159
+        System.out.printf("The value of pi is approximately " + "%.2f.", pi);// my 2nd option way yield 3.14
+        System.out.format("The value of pi is approximately " + "%.2f.", pi);// instructor way yields 3.14
 
-        Scanner scanner = new Scanner(System.in);
 
+
+        Scanner scanner = new Scanner(System.in);// Scanner class + variable name = syntax"new Scanner(System.in);
         //TODO:prompt user to enter integer, store in int variable using nextInt
         System.out.print("\nEnter a number: ");
-        int userInput = scanner.nextInt();
+        int userInput = scanner.nextInt();// scanner here is just the variable name
         System.out.println("You entered : --> \"" + userInput + "\" <--");
         //above requires an integer
 
         //TODO: Prompt user to enter 3 words, store in separate variables
-        System.out.print("\nWhat are your three favorite colors: ");
+        System.out.println("\nWhat are your three favorite colors: ");
         String color1 = scanner.next();
         String color2 = scanner.next();
         String color3 = scanner.next();
         System.out.printf("Your three favorite colors are: " + "\n%s\n, %s\n, %s", color1, color2, color3);
 
         //TODO: enter a sentence and store in string variable
-        System.out.print("\nWrite a sentence: ");
-        String userSentence = scanner.next();
+        System.out.println("\nWrite a sentence: ");
+//        String userSentence = scanner.next();
         String userSentence1 = scanner.nextLine();
-        System.out.printf("\nYour first word is: " + userSentence);// returns first word
-        System.out.printf("\nThe rest of the sentence is: " + userSentence1);// returns rest of sentence
-        System.out.printf("\nThe whole sentence: " + userSentence + userSentence1);
+//        System.out.printf("\nYour first word is: " + userSentence);// returns first word
+//        System.out.printf("\nThe rest of the sentence is: " + userSentence1);// returns rest of sentence
+//        System.out.printf("\nThe whole sentence: " + userSentence + userSentence1);
+        System.out.printf(userSentence1);
 
         //Calculate the perimeter and area of Codeup's classrooms.
-        System.out.print("\nEnter the length and width of our Codeup classroom? ");
-        String length = scanner.nextLine();
-        String width = scanner.nextLine();
+        //TODO
+        System.out.println("\nEnter the length and width of our Codeup classroom?");
+//        String length = scanner.nextLine();
+//        String width = scanner.nextLine();
 //        int perimeter = Integer.parseInt(length + width + length + width);
 //        System.out.print(dimensions);
+        System.out.println("\nEnter the width of our Codeup classroom?");
+        double width = Double.parseDouble(scanner.nextLine());// nextline is a string, parsing into numeric
 
+        System.out.println("\nEnter the length of our Codeup classroom?");
+        double length = Double.parseDouble(scanner.nextLine());
+
+        double area = length * width;
+        double perimeter = (2*width)+(2*length);
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+
+        //TODO Try the bonuses Next
     }
 }

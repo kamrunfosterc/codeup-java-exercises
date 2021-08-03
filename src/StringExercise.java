01eb062db7836ea10dddf9febc743eee79eb83f9
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-03 11:47:54 -0500

    StringExercise complete, working on bob class next

---
+++ src/StringExercise.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Locale;
+public class StringExercise {
+    public static void main(String[] args) {
+        //TODO 1) String Basics
+        String noEdu = "We don't need no education. \nWe don't need no thought control";
+        System.out.println(noEdu);
+
+        String checkThisOut = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(checkThisOut);
+
+        String windows = "In windows, the main drive is usually C:\\";
+        System.out.println(windows);
+
+        String backslashes = "I can do backslashes \\, double backslashes \\\\,\n" +
+                "and the amazing triple backslash \\\\\\!";
+        System.out.println(backslashes);
+
+    }
+}
+
---
+++ src/String_Lec_Exercises.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+import java.lang.Math;
+import java.util.Locale;
+public class String_Lec_Exercises {
+    public static void main(String[] args) {
+//        // strings are objects (ref types) not primitive and cannot be compared using ==
+//        //Example 1: Immutability and Concatenation
+//        String message = "Hello";
+//        System.out.println(message);
+//        message = message + "World!";
+//        System.out.println(message);
+//
+//        //Example 2: Comparing Strings
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Continue? [y/n] ");
+//        String userInput = sc.next();
+//
+//        // DO NOT DO THIS
+//        boolean confirmation = userInput == "y";
+//        System.out.println(confirmation);
+//        //DO THIS!!
+//        boolean correctConfirmation = userInput.equals("y");
+//        System.out.println(correctConfirmation);
+//
+//        //TODO String Comparison Methods
+//
+//        //EXAMPLE 3: String Comparison Mthods
+//        String input = "Codeup Rocks!";// can add trailing/ leading white spaces to work with trim()
+////
+////        input.equals("codeup rocks!"); // false
+////        input.equals("Codeup Rocks!"); // true
+////
+////        input.equalsIgnoreCase("codeup rocks!"); // true
+////        input.equalsIgnoreCase("Codeup Rocks!"); // true
+////
+////        input.startsWith("codeup"); // false
+////        input.startsWith("Codeup"); // true
+////
+////        input.endsWith("rocks"); // false
+////        input.endsWith("rocks!"); // false
+////        input.endsWith("Rocks!"); // true
+//
+//        //TODO String Manipulation Methods
+////        char charAt(int index);//Returns the character at the specified index of the string.
+//        System.out.println(input.charAt(3));
+//
+////        int indexOf(String subString);
+//        //Returns the index of the first occurrence of a certain substring.
+//        //Returns -1 if the substring is not found.
+//        System.out.println(input.indexOf("c"));// returns 9
+//        //if substring isn't in string returns -1
+//        System.out.println(input.indexOf("f"));
+//
+////        int lastIndexOf(String subString);//Like indexOf, but starts the search from the end of the string.
+//        System.out.println(input.lastIndexOf("o"));
+//
+////        int length();//Returns the length of a string.
+//        System.out.println(input.length());// returns 13
+//
+////        String replace(String pattern, String replacement);Returns a copy of the string that has pattern replaced with replacement.
+//        String replaceSentence = "I love teh color red. I love the color blue. I love yellow";
+//        System.out.println(replaceSentence.replace("love", "like"));
+//
+////        String substring(int beginIndex[, int endIndex])//Returns a new substring that starts at a specified index and (optionally) ends at the specified index
+//        System.out.println(input.substring(0, 6));// returns 1st 6 characters, Codeup
+//
+////        String toLowerCase()//Returns a string that has all lower case letters.
+//        System.out.println(input.toLowerCase());
+//
+////        String toUpperCase()//Returns a string that has all upper case letters.
+//        System.out.println(input.toUpperCase());
+//
+////        String trim();Returns a copy of the string without leading and trailing whitespaces.
+//        // good to add on to password strings to eliminate the password spaces
+//        System.out.println(input.trim());
+
+
+
+    }
+}
+

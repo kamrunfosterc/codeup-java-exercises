651b4b1b2390c113a827bb8a70e40038d61ae5ee
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-03 13:59:09 -0500

    walk through with additional options added, will add switch case shortly for additional references

--- src/Bob.java
+++ src/Bob.java
@@ -31,7 +31,35 @@ public class Bob {
             System.out.println("Whatever");
         }
 
+        //TODO Telvin version of working on it
 
+//        boolean toContinue = true;
+//        do{
+//            String userInput = scanner.nextLine();
+//            //if statement here
+//        }while(toContinue);
+
+//        boolean toContinue = true;
+//        do {
+//            String userInput = scanner.nextLine();
+//            if (userInput.endsWith("?")) {
+//                System.out.println("Sure.");
+//            } else if (userInput.endsWith("!")){
+//                System.out.println("Whoa, chill out!");
+//            } else if (userInput.equals("")) {
+//                System.out.println("Be that way!");
+//            } else {
+//                System.out.println("Whatever.");
+//            }
+//            System.out.println("Would you like to continue to text Bob? (y or n)?");
+//            String userResponse = scanner.nextLine();
+//            if (!userResponse.equals("y")) {
+//                toContinue = false;
+//                System.out.println("Good-Bye!");
+//            } else {
+//                System.out.println("Text to talk to Bob: ");
+//            }
+//        } while (toContinue);
 
 
 

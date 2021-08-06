361868ddc12953925085a5275bba962214cc6dc6
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-06 10:33:19 -0500

    updated

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,7 @@
 public class Person {
 
     private String name;
-
+    // cmd n to create constructors
     public Person(String name){
         System.out.println("Person Constructor being used!");
         this.name = name;
@@ -24,7 +24,6 @@ public class Person {
     Create a `main` method on the class that creates a new `Person` object and
     tests the above methods.*/
 
-
     public static void main(String[] args) {
         //TODO: Part 1 object basics
         Person person = new Person("name");
@@ -35,20 +34,20 @@ public class Person {
         person.sayHello();*/
 
         //TODO Part 1 Understanding references
-        Person person1 = new Person("Kamrun");
+        Person person1 = new Person("Kamrun");// person instance, object
         Person person2 = new Person("Kamrun");
         System.out.println(person1.getName().equals(person2.getName()));
-        System.out.println("Jingle");
+        System.out.println("Jingle");// extra to seperate and see where the diff lies
         System.out.println(person1 == person2);// expected true showing false, not sure why yet
 
         Person person3 = new Person("Amari");
         Person person4 = person3;
-        System.out.println("Is person4 the same as person3? \n" + (person3 == person4) + "!!!");
+        System.out.println("Is person4 the same as person3? \n" + (person3 == person4) + "!!!");//true
 
-        Person person5 = new Person("July");
-        Person person6 = person5;
-        System.out.println(person5.getName());
-        System.out.println(person6.getName());
+        Person person5 = new Person("July");// one unique person, object
+        Person person6 = person5;// person 6 is the same instance of person 5
+        System.out.println(person5.getName());// returns july
+        System.out.println(person6.getName());// returns another july
         person6.setName("Mr. Carson Williams");// changing name of person 6
         System.out.println("Who are you? "+ person5.getName());// name changed
         System.out.println("What is your new name? "+ person6.getName());// name changed as well
--- src/util/Input.java
+++ src/util/Input.java
@@ -1,10 +1,37 @@
 package util;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Input {
     //TODO Part 1 Create an input validation class
 
-    private String scanner;
-//    Scanner scanner = new Scanner();
+    //
+    private Scanner scanner;// initialized variable for when instance is created
+
+    public Input(){// no argument constructor
+        System.out.println("creating new input instance.");// extra line, can comment out
+        this.scanner = new Scanner(System.in);// each instance
+        // created scanner so when we estanciate a new input class it'll read scanner
+    }
+    public String getString(){
+        System.out.println("Do something: ");// prompts for info
+        String personInput = scanner.nextLine();// stores types info in variable
+        return personInput;
+    }
+    public boolean yesNo(){
+        System.out.println("Do you like to fly? (Yes/No) ");
+        String personAnswer = scanner.nextLine();// stores user input in variable
+//        if (personAnswer.toLowerCase().contains("y")){// alternative way
+//            return true;
+//        } else {
+//            return false;
+//        }
+        return personAnswer.contains("y");// return true if y is in answer
+    }
+    public int getInt(int min, int max){}
+    public int getInt(){}
+    public double getDouble(double min, double max){}
+    double getDouble(){}
 
 //    public start here
 

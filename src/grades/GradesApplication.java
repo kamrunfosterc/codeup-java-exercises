5a2f0c4682707a1437df18385c0eab659eb2fb1f
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-11 18:42:25 -0500

    updated collections exercise

--- src/grades/GradesApplication.java
+++ src/grades/GradesApplication.java
@@ -19,37 +19,48 @@ public class GradesApplication {
         //todo ADDED students and 3 grades per student
         students.put("Jsun78",studentJohnny);//created relationship between these two values
         System.out.println(students.get("Jsun78"));// exist in hashmap
-        studentJohnny.addGrade(88);
-        studentJohnny.addGrade(72);
-        studentJohnny.addGrade(95);
+        studentJohnny.addGrade(84);
+        studentJohnny.addGrade(80);
+        studentJohnny.addGrade(91);
         System.out.println("Johnny's grade average: "+ studentJohnny.getGradeAverage());
 
         students.put("Kfos0561!",studentKamrun);
-        studentKamrun.addGrade(88);
-        studentKamrun.addGrade(72);
-        studentKamrun.addGrade(95);
+        studentKamrun.addGrade(81);
+        studentKamrun.addGrade(79);
+        studentKamrun.addGrade(89);
+        System.out.println("Kamrun's grade average: "+ studentKamrun.getGradeAverage());
 
         students.put("Jser768$",studentJasmine);
         studentJasmine.addGrade(88);
         studentJasmine.addGrade(72);
         studentJasmine.addGrade(95);
+        System.out.println("Jasmine's grade average: "+ studentJasmine.getGradeAverage());
 
         students.put("XB17!7",studentHarper);
-        studentHarper.addGrade(88);
-        studentHarper.addGrade(72);
-        studentHarper.addGrade(95);
+        studentHarper.addGrade(54);
+        studentHarper.addGrade(73);
+        studentHarper.addGrade(68);
+        System.out.println("Harper's grade average: "+ studentHarper.getGradeAverage());
 
         students.put("ML26#",studentMaryLou);
-        studentMaryLou.addGrade(88);
-        studentMaryLou.addGrade(72);
-        studentMaryLou.addGrade(95);
+        studentMaryLou.addGrade(80);
+        studentMaryLou.addGrade(75);
+        studentMaryLou.addGrade(91);
+        System.out.println("Mary Lou's grade average: "+ studentMaryLou.getGradeAverage());
+
 
         students.put("sbell99@",studentAnnaMae);
-        studentAnnaMae.addGrade(88);
-        studentAnnaMae.addGrade(72);
+        studentAnnaMae.addGrade(80);
+        studentAnnaMae.addGrade(68);
         studentAnnaMae.addGrade(95);
+        System.out.println("AnnaMae's grade average: "+ studentAnnaMae.getGradeAverage());
 
         students.put("Ceribro45",studentAshley);
+        studentAshley.addGrade(90);
+        studentAshley.addGrade(82);
+        studentAshley.addGrade(98);
+        System.out.println("Ashley's grade average: "+ studentAshley.getGradeAverage());
+
 
 
         //todo think about how we can add more students w/out the need to do the above

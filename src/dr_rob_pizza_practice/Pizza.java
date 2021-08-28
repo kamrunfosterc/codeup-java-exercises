1f7d8224ab68985bb35db177e25b91fc40d09fa2
Kamrun Foster <kamrun.foster4@gmail.com>
2021-08-28 14:03:12 -0500

    walk through

--- src/dr_rob_pizza_practice/Pizza.java
+++ src/dr_rob_pizza_practice/Pizza.java
@@ -4,6 +4,54 @@ import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 public class Pizza {
+    // TODO:############  8/28/21  ############
+    private String pizzaType;
+    private String size;
+    private String crustType;
+
+    public Pizza(String pizzaType, String size, String crustType) {
+        this.pizzaType = pizzaType;
+        this.size = size;
+        this.crustType = crustType;
+    }
+
+    @Override
+    public String toString(){
+        String crustText = crustType;
+        return size+" "+ getCrustType()+" "+pizzaType+" pizza";
+    }
+    //ACCESSORS
+
+    public String getPizzaType() {
+        return pizzaType;
+    }
+
+    public void setPizzaType(String pizzaType) {
+        this.pizzaType = pizzaType;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getCrustType() {
+        if (crustType.equalsIgnoreCase("thin") ||crustType.equalsIgnoreCase("hand-tossed")){
+            return crustType + "crust";
+        }
+        return crustType;
+
+    }
+
+    public void setCrustType(String crustType) {
+        this.crustType = crustType;
+    }
+
+
+    //  TODO:############ 8/28/21  ############
 
     // TODO: 8/24/21  DR Rob practise
     //    Make a dr_rob_pizza_practice.Pizza class. It should have fields like pizza type (e.g., Cheese, Pepperoni, Supreme, etc.),
@@ -17,50 +65,59 @@ public class Pizza {
 //        Next, write a printPizza method that takes an ArrayList of dr_rob_pizza_practice.Pizza and prints out the pizzas in the ArrayList.
 //        In your main, call makePizza and then call printPizza
 
-    // pizza type field
-    private String pizzaType;// Cheese, Pepperoni, Margarita
-    private String pizzaSize;// Small 6", Medium 9", Large 12", and Extra Large 15" MIGHT CHANGE TO INT
-    private String pizzaCrust;//hin, Hand-tossed, Pan
 
-    public Pizza(String pizzaType, String pizzaSize, String pizzaCrust) { // todo created constructor
-        this.pizzaType = pizzaType;
-        this.pizzaSize = pizzaSize;
-        this.pizzaCrust = pizzaCrust;
-    }
 
-    public String getPizzaType() {
-        return pizzaType;
-    }
 
-    public void setPizzaType(String pizzaType) {
-        this.pizzaType = pizzaType;
-    }
 
-    public String getPizzaSize() {
-        return pizzaSize;
-    }
 
-    public void setPizzaSize(String pizzaSize) {
-        this.pizzaSize = pizzaSize;
-    }
 
-    public String getPizzaCrust() {
-        return pizzaCrust;
-    }
 
-    public void setPizzaCrust(String pizzaCrust) {
-        this.pizzaCrust = pizzaCrust;
-    }
 
-    //    public String pizzaSize (String size){
-//        Scanner userEntry = new Scanner(System.in);
-//        System.out.println("Please choose your pizza size: small, medium, large, extra large >> "); //todo STARTING HERE
-//        String chosenSize = userEntry.nextLine();
-//        if (size == 'small'){
-//            "Chosen dr_rob_pizza_practice.Pizza Size: "
-//        }
+//    // TODO: 8/28/21 KAMRUN'S WORK BELOW
+//    // pizza type field
+//    private String pizzaType;// Cheese, Pepperoni, Margarita
+//    private String pizzaSize;// Small 6", Medium 9", Large 12", and Extra Large 15" MIGHT CHANGE TO INT
+//    private String pizzaCrust;//hin, Hand-tossed, Pan
 //
+//    public Pizza(String pizzaType, String pizzaSize, String pizzaCrust) { // todo created constructor
+//        this.pizzaType = pizzaType;
+//        this.pizzaSize = pizzaSize;
+//        this.pizzaCrust = pizzaCrust;
 //    }
+//
+//    public String getPizzaType() {
+//        return pizzaType;
+//    }
+//
+//    public void setPizzaType(String pizzaType) {
+//        this.pizzaType = pizzaType;
+//    }
+//
+//    public String getPizzaSize() {
+//        return pizzaSize;
+//    }
+//
+//    public void setPizzaSize(String pizzaSize) {
+//        this.pizzaSize = pizzaSize;
+//    }
+//
+//    public String getPizzaCrust() {
+//        return pizzaCrust;
+//    }
+//
+//    public void setPizzaCrust(String pizzaCrust) {
+//        this.pizzaCrust = pizzaCrust;
+//    }
+//
+//    //    public String pizzaSize (String size){
+////        Scanner userEntry = new Scanner(System.in);
+////        System.out.println("Please choose your pizza size: small, medium, large, extra large >> "); //todo STARTING HERE
+////        String chosenSize = userEntry.nextLine();
+////        if (size == 'small'){
+////            "Chosen dr_rob_pizza_practice.Pizza Size: "
+////        }
+////
+////    }
 
 
 
--- src/dr_rob_pizza_practice/PizzaShop.java
+++ src/dr_rob_pizza_practice/PizzaShop.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class PizzaShop extends Pizza{
-
-    public PizzaShop(String pizzaType, String pizzaSize, String pizzaCrust) {
-        super(pizzaType, pizzaSize, pizzaCrust);
-    }
+    
+    //TODO  KAMRUN WORK 
+//    public PizzaShop(String pizzaType, String pizzaSize, String pizzaCrust) {
+//        super(pizzaType, pizzaSize, pizzaCrust);
+//    }
     // TODO: 8/27/21
 //        Make a PizzaShop class. This class will have your main() method for the program.
 //        Write a static method in the PizzaShop class to make some pizza. The method should return an ArrayList of
@@ -16,30 +17,74 @@ public class PizzaShop extends Pizza{
 //        Next, write a printPizza method that takes an ArrayList of dr_rob_pizza_practice.Pizza and prints out the pizzas in the ArrayList.
 //        In your main, call makePizza and then call printPizza
 
-    public static ArrayList<String> makePizza(){
-        return makePizza();
-    }
-
+//    public static ArrayList<String> makePizza(){
+//        return makePizza();
+//    }
+    public static ArrayList<Pizza> pizzas;
+    
     public static void main(String[] args) {
-        PizzaShop.makePizza();// not sure what to do here.... stopped 12:31
-        ArrayList<String> pizza = new ArrayList<>();
-        Scanner userEntry = new Scanner(System.in);
+        
+        //// TODO: 8/28/21 ASKING FOR USER INPUT SHOULD BE PUT INTO A LOOP 
+//        Pizza pizza1 = new Pizza("Pepperoni", "large", "thin");
+//        Pizza pizza2 = new Pizza("cheese", "small", "hand-tossed");
+//        Pizza pizza3 = new Pizza("supreme", "extra-large", "pan");
+        
+//        pizzas = new ArrayList<>();
+//        pizzas.add(pizza1);
+//        pizzas.add(pizza2);
+//        pizzas.add(pizza3);
+//        
+//        printPizzas(pizzas);
+        
+        ArrayList<Pizza> pizzaStack1 = makePizzas();
+        ArrayList<Pizza> pizzaStack2 = makePizzas();
+        
+        pizzaStack2.remove(pizzaStack2.size()-1);
+        
+        printPizzas(pizzaStack1);
+        printPizzas(pizzaStack2);
+        
+//        // TODO: 8/28/21 KAMRUN WORK BELOW 
+//        PizzaShop.makePizza();// not sure what to do here.... stopped 12:31
+//        ArrayList<String> pizza = new ArrayList<>();
+//        Scanner userEntry = new Scanner(System.in);
+//
+//        System.out.println("What type of pizza would you like, \n\t*** select one ***\n [Cheese, Pepperoni, Margherita] ");
+//        String chosenType = userEntry.nextLine();
+//        System.out.println("\nWhat size pizza do you prefer:\n [Small, Medium, Large , or Extra Large ] ");
+//        String chosenSize = userEntry.nextLine();
+//        System.out.println("\nPlease enter the type of crust you prefer \n[thin, Hand-tossed, Pan] ");
+//        String chosenCrust = userEntry.nextLine();
+//
+//
+//        pizza.add(chosenType);
+//        pizza.add(chosenSize);
+//        pizza.add(chosenCrust);
+//
+//
+//        System.out.println("Current pizza order"+"\n"+pizza);
+////        System.out.printf(chosenType + ' ' + chosenSize + ' ' + chosenCrust);// testing to make sure user entries were returned
 
-        System.out.println("What type of pizza would you like, \n\t*** select one ***\n [Cheese, Pepperoni, Margherita] ");
-        String chosenType = userEntry.nextLine();
-        System.out.println("\nWhat size pizza do you prefer:\n [Small, Medium, Large , or Extra Large ] ");
-        String chosenSize = userEntry.nextLine();
-        System.out.println("\nPlease enter the type of crust you prefer \n[thin, Hand-tossed, Pan] ");
-        String chosenCrust = userEntry.nextLine();
-
-
-        pizza.add(chosenType);
-        pizza.add(chosenSize);
-        pizza.add(chosenCrust);
-
-
-        System.out.println("Current pizza order"+"\n"+pizza);
-//        System.out.printf(chosenType + ' ' + chosenSize + ' ' + chosenCrust);// testing to make sure user entries were returned
-
+    }
+    public static ArrayList<Pizza> makePizzas(){
+        ArrayList<Pizza> pizzas = new ArrayList<>();
+        
+        Pizza pizza1 = new Pizza("Pepperoni", "large", "thin");
+        Pizza pizza2 = new Pizza("cheese", "small", "hand-tossed");
+        Pizza pizza3 = new Pizza("supreme", "extra-large", "pan");
+        
+        pizzas = new ArrayList<>();
+        pizzas.add(pizza1);
+        pizzas.add(pizza2);
+        pizzas.add(pizza3);
+        
+        return pizzas;
+    }
+    
+    private static void printPizzas(ArrayList<Pizza> pizzas){
+        System.out.println("We currently have the following pizzas for sale: ");
+        for (Pizza pizza: pizzas){
+            System.out.println(pizza);
+        }
     }
 }
